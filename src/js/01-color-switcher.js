c3298dfe9f79535abf0d94e755fa9af4b90581a6
Tyoma <risendrums777@gmail.com>
2021-11-04 00:28:53 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+btnStop.setAttribute('disabled', true);
+let timerId = null;
+
+btnStart.addEventListener('click', launchRandomColor);
+btnStop.addEventListener('click', stopRandomColor);
+
+function launchRandomColor() {
+  timerId = setInterval(() => {
+    document.body.style.background = `${getRandomHexColor()}`;
+    btnStart.setAttribute('disabled', true);
+    btnStop.removeAttribute('disabled');
+  }, 1000);
+}
+
+function stopRandomColor() {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+}
